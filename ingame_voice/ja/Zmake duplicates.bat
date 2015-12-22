840fb2bb84c9ac5375fc7e554bb182bf1f3546a6
Willster419 <willster419@outlook.com>
2015-12-22 01:15:11 -0500

    for 9.13 - updated it so it is compatible with the national voices. this also means that the ingame voices can be changed from within the game/battle.

---
+++ ingame_voice/ja/Zmake duplicates.bat
@@ -0,0 +1,88 @@
+copy ammo_bay_damaged_04.wav ammo_bay_damaged_05.wav
+
+copy armor_not_pierced_by_player_01.wav armor_not_pierced_by_player_06.wav
+copy armor_not_pierced_by_player_02.wav armor_not_pierced_by_player_07.wav
+copy armor_not_pierced_by_player_03.wav armor_not_pierced_by_player_08.wav
+copy armor_not_pierced_by_player_04.wav armor_not_pierced_by_player_09.wav
+copy armor_not_pierced_by_player_05.wav armor_not_pierced_by_player_10.wav
+copy armor_not_pierced_by_player_06.wav armor_not_pierced_by_player_11.wav
+copy armor_not_pierced_by_player_07.wav armor_not_pierced_by_player_12.wav
+
+copy armor_pierced_by_player_01.wav armor_pierced_by_player_06.wav
+copy armor_pierced_by_player_02.wav armor_pierced_by_player_07.wav
+copy armor_pierced_by_player_03.wav armor_pierced_by_player_08.wav
+copy armor_pierced_by_player_04.wav armor_pierced_by_player_09.wav
+copy armor_pierced_by_player_05.wav armor_pierced_by_player_10.wav
+copy armor_pierced_by_player_06.wav armor_pierced_by_player_11.wav
+copy armor_pierced_by_player_07.wav armor_pierced_by_player_12.wav
+copy armor_pierced_by_player_08.wav armor_pierced_by_player_13.wav
+copy armor_pierced_by_player_09.wav armor_pierced_by_player_14.wav
+
+copy armor_pierced_crit_by_player_01.wav armor_pierced_crit_by_player_04.wav
+copy armor_pierced_crit_by_player_02.wav armor_pierced_crit_by_player_05.wav
+
+copy damage_by_near_explosion_by_player_01.wav damage_by_near_explosion_by_player_06.wav
+copy damage_by_near_explosion_by_player_02.wav damage_by_near_explosion_by_player_07.wav
+copy damage_by_near_explosion_by_player_03.wav damage_by_near_explosion_by_player_08.wav
+copy damage_by_near_explosion_by_player_04.wav damage_by_near_explosion_by_player_09.wav
+
+copy enemy_killed_by_player_01.wav enemy_killed_by_player_11.wav
+copy enemy_killed_by_player_02.wav enemy_killed_by_player_12.wav
+copy enemy_killed_by_player_03.wav enemy_killed_by_player_13.wav
+copy enemy_killed_by_player_04.wav enemy_killed_by_player_14.wav
+
+copy start_battle_01.wav start_battle_22.wav
+copy start_battle_02.wav start_battle_23.wav
+copy start_battle_03.wav start_battle_24.wav
+copy start_battle_04.wav start_battle_25.wav
+copy start_battle_05.wav start_battle_26.wav
+copy start_battle_06.wav start_battle_27.wav
+copy start_battle_07.wav start_battle_28.wav
+copy start_battle_08.wav start_battle_29.wav
+copy start_battle_09.wav start_battle_30.wav
+copy start_battle_10.wav start_battle_31.wav
+copy start_battle_11.wav start_battle_32.wav
+copy start_battle_12.wav start_battle_33.wav
+copy start_battle_13.wav start_battle_34.wav
+copy start_battle_14.wav start_battle_35.wav
+copy start_battle_15.wav start_battle_36.wav
+copy start_battle_16.wav start_battle_37.wav
+copy start_battle_17.wav start_battle_38.wav
+copy start_battle_18.wav start_battle_39.wav
+copy start_battle_19.wav start_battle_40.wav
+copy start_battle_20.wav start_battle_41.wav
+copy start_battle_21.wav start_battle_42.wav
+copy start_battle_22.wav start_battle_43.wav
+copy start_battle_23.wav start_battle_44.wav
+copy start_battle_24.wav start_battle_45.wav
+copy start_battle_25.wav start_battle_46.wav
+copy start_battle_26.wav start_battle_47.wav
+copy start_battle_27.wav start_battle_48.wav
+
+copy target_lost_01.wav target_lost_05.wav
+copy target_lost_02.wav target_lost_06.wav
+
+copy track_destroyed_01.wav track_destroyed_05.wav
+copy track_destroyed_02.wav track_destroyed_06.wav
+
+copy vehicle_destroyed_01.wav vehicle_destroyed_06.wav
+copy vehicle_destroyed_02.wav vehicle_destroyed_07.wav
+copy vehicle_destroyed_03.wav vehicle_destroyed_08.wav
+copy vehicle_destroyed_04.wav vehicle_destroyed_09.wav
+copy vehicle_destroyed_05.wav vehicle_destroyed_10.wav
+copy vehicle_destroyed_06.wav vehicle_destroyed_11.wav
+copy vehicle_destroyed_07.wav vehicle_destroyed_12.wav
+copy vehicle_destroyed_08.wav vehicle_destroyed_13.wav
+copy vehicle_destroyed_09.wav vehicle_destroyed_14.wav
+copy vehicle_destroyed_10.wav vehicle_destroyed_15.wav
+copy vehicle_destroyed_11.wav vehicle_destroyed_16.wav
+copy vehicle_destroyed_12.wav vehicle_destroyed_17.wav
+copy vehicle_destroyed_13.wav vehicle_destroyed_18.wav
+copy vehicle_destroyed_14.wav vehicle_destroyed_19.wav
+copy vehicle_destroyed_15.wav vehicle_destroyed_20.wav
+copy vehicle_destroyed_16.wav vehicle_destroyed_21.wav
+copy vehicle_destroyed_17.wav vehicle_destroyed_22.wav
+copy vehicle_destroyed_18.wav vehicle_destroyed_23.wav
+copy vehicle_destroyed_18.wav vehicle_destroyed_24.wav
+copy vehicle_destroyed_20.wav vehicle_destroyed_25.wav
+copy vehicle_destroyed_21.wav vehicle_destroyed_26.wav
