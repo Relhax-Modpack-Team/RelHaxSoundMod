45917788b442e435efa7cf6100fd7c53cc0f9020
Willster419 <willster419@outlook.com>
2016-05-07 02:27:43 -0400

    finally moved relHax to 0.9.14 with wwise support!

---
+++ ingame voice/Base Pack/RelHaxCen/Zexport audio banks.bat
@@ -0,0 +1,9 @@
+
+copy RelHax_1\GeneratedSoundBanks\Windows\English(UK)\RelHax_1.bnk export\uk\RelHax_1.bnk
+copy RelHax_2\GeneratedSoundBanks\Windows\English(UK)\RelHax_2.bnk export\uk\RelHax_2.bnk
+copy RelHax_3\GeneratedSoundBanks\Windows\English(UK)\RelHax_3.bnk export\uk\RelHax_3.bnk
+copy RelHax_4\GeneratedSoundBanks\Windows\English(UK)\RelHax_4.bnk export\uk\RelHax_4.bnk
+copy RelHax_1\GeneratedSoundBanks\Windows\English(US)\RelHax_1.bnk export\usa\RelHax_1.bnk
+copy RelHax_2\GeneratedSoundBanks\Windows\English(US)\RelHax_2.bnk export\usa\RelHax_2.bnk
+copy RelHax_3\GeneratedSoundBanks\Windows\English(US)\RelHax_3.bnk export\usa\RelHax_3.bnk
+copy RelHax_4\GeneratedSoundBanks\Windows\English(US)\RelHax_4.bnk export\usa\RelHax_4.bnk
