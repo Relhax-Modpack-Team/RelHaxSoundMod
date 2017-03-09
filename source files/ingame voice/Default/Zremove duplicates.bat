0712a6d7d3285dd31f2167d8c41518d7baa9a47d
Willster419 <willster419@outlook.com>
2017-03-08 22:25:22 -0500

    updated source files

---
+++ source files/ingame voice/Default/Zremove duplicates.bat
@@ -0,0 +1,103 @@
+
+del armor_not_pierced_by_player_06.wav
+del armor_not_pierced_by_player_07.wav
+del armor_not_pierced_by_player_08.wavdel armor_not_pierced_by_player_09.wav
+del armor_not_pierced_by_player_10.wav
+del armor_not_pierced_by_player_11.wav
+del armor_not_pierced_by_player_12.wav
+del armor_not_pierced_by_player_13.wav
+del armor_not_pierced_by_player_14.wav
+del armor_not_pierced_by_player_15.wav
+
+del armor_pierced_by_player_06.wav
+del armor_pierced_by_player_07.wav
+del armor_pierced_by_player_08.wav
+del armor_pierced_by_player_09.wav
+del armor_pierced_by_player_10.wav
+del armor_pierced_by_player_11.wav
+del armor_pierced_by_player_12.wav
+del armor_pierced_by_player_13.wav
+del armor_pierced_by_player_14.wav
+
+del armor_ricochet_by_player_13.wav
+del armor_ricochet_by_player_14.wav
+del armor_ricochet_by_player_15.wav
+
+del damage_by_near_explosion_by_player_06.wav
+del damage_by_near_explosion_by_player_07.wav
+del damage_by_near_explosion_by_player_08.wav
+del damage_by_near_explosion_by_player_09.wav
+
+del enemy_killed_by_player_11.wav
+del enemy_killed_by_player_12.wav
+del enemy_killed_by_player_13.wav
+del enemy_killed_by_player_14.wav
+del enemy_killed_by_player_15.wav
+del enemy_killed_by_player_16.wav
+
+del start_battle_21.wav
+del start_battle_22.wav
+del start_battle_23.wav
+del start_battle_24.wav
+del start_battle_25.wav
+del start_battle_26.wav
+del start_battle_27.wav
+del start_battle_28.wav
+del start_battle_29.wav
+del start_battle_30.wav
+del start_battle_31.wav
+del start_battle_32.wav
+del start_battle_33.wav
+del start_battle_34.wav
+del start_battle_35.wav
+del start_battle_36.wav
+del start_battle_37.wav
+del start_battle_38.wav
+del start_battle_39.wav
+del start_battle_40.wav
+del start_battle_41.wav
+del start_battle_42.wav
+del start_battle_43.wav
+del start_battle_44.wav
+del start_battle_45.wav
+del start_battle_46.wav
+del start_battle_47.wav
+del start_battle_48.wav
+del start_battle_49.wav
+del start_battle_50.wav
+del start_battle_51.wav
+del start_battle_52.wav
+del start_battle_53.wav
+del start_battle_54.wav
+del start_battle_55.wav
+del start_battle_56.wav
+
+del target_lost_05.wav
+del target_lost_06.wav
+
+del track_destroyed_05.wav
+del track_destroyed_06.wav
+
+del vehicle_destroyed_06.wav
+del vehicle_destroyed_07.wav
+del vehicle_destroyed_08.wav
+del vehicle_destroyed_09.wav
+del vehicle_destroyed_10.wav
+del vehicle_destroyed_11.wav
+del vehicle_destroyed_12.wav
+del vehicle_destroyed_13.wav
+del vehicle_destroyed_14.wav
+del vehicle_destroyed_15.wav
+del vehicle_destroyed_16.wav
+del vehicle_destroyed_17.wav
+del vehicle_destroyed_18.wav
+del vehicle_destroyed_19.wav
+del vehicle_destroyed_20.wav
+del vehicle_destroyed_21.wav
+del vehicle_destroyed_22.wav
+del vehicle_destroyed_23.wav
+del vehicle_destroyed_24.wav
+del vehicle_destroyed_25.wav
+del vehicle_destroyed_26.wav
+del vehicle_destroyed_27.wav
+del vehicle_destroyed_28.wav
---
+++ source files/wwise_project_9.17.1/Originals/Voices/English(US)/Zmake duplicates.bat
@@ -0,0 +1,114 @@
+
+copy armor_not_pierced_by_player_01.wav armor_ricochet_by_player_01.wav
+copy armor_not_pierced_by_player_02.wav armor_ricochet_by_player_02.wav
+copy armor_not_pierced_by_player_03.wav armor_ricochet_by_player_03.wav
+copy armor_not_pierced_by_player_04.wav armor_ricochet_by_player_04.wav
+copy armor_not_pierced_by_player_05.wav armor_ricochet_by_player_05.wav
+copy armor_not_pierced_by_player_06.wav armor_ricochet_by_player_06.wav
+copy armor_not_pierced_by_player_07.wav armor_ricochet_by_player_07.wav
+copy armor_not_pierced_by_player_08.wav armor_ricochet_by_player_08.wav
+copy armor_not_pierced_by_player_09.wav armor_ricochet_by_player_09.wav
+copy armor_not_pierced_by_player_10.wav armor_ricochet_by_player_10.wav
+copy armor_not_pierced_by_player_11.wav armor_ricochet_by_player_11.wav
+copy armor_not_pierced_by_player_12.wav armor_ricochet_by_player_12.wav
+copy armor_not_pierced_by_player_13.wav armor_ricochet_by_player_13.wav
+copy armor_not_pierced_by_player_14.wav armor_ricochet_by_player_14.wav
+copy armor_not_pierced_by_player_15.wav armor_ricochet_by_player_15.wav
+
+copy crew_deactivated_01.wav crew_deactivated_03.wav
+copy crew_deactivated_02.wav crew_deactivated_04.wav
+copy crew_deactivated_03.wav crew_deactivated_05.wav
+
+copy damage_by_near_explosion_by_enemy_01.wav damage_by_near_explosion_by_enemy_02.wav
+
+copy driver_killed_01.wav driver_killed_03.wav
+copy driver_killed_02.wav driver_killed_04.wav
+
+copy enemy_fire_started_by_player_01.wav enemy_fire_started_by_player_03.wav
+copy enemy_fire_started_by_player_02.wav enemy_fire_started_by_player_04.wav
+copy enemy_fire_started_by_player_03.wav enemy_fire_started_by_player_05.wav
+
+copy engine_damaged_01.wav engine_damaged_03.wav
+copy engine_damaged_02.wav engine_damaged_04.wav
+copy engine_damaged_03.wav engine_damaged_05.wav
+copy engine_damaged_04.wav engine_damaged_06.wav
+copy engine_damaged_05.wav engine_damaged_07.wav
+
+copy engine_destroyed_01.wav engine_destroyed_03.wav
+
+copy engine_functional_01.wav engine_functional_03.wav
+copy engine_functional_02.wav engine_functional_04.wav
+
+copy fire_started_01.wav fire_started_05.wav
+copy fire_started_02.wav fire_started_06.wav
+copy fire_started_03.wav fire_started_07.wav
+copy fire_started_04.wav fire_started_08.wav
+
+copy fire_stopped_01.wav fire_stopped_03.wav
+copy fire_stopped_02.wav fire_stopped_04.wav
+copy fire_stopped_03.wav fire_stopped_05.wav
+
+copy fuel_tank_damaged_01.wav fuel_tank_damaged_03.wav
+copy fuel_tank_damaged_02.wav fuel_tank_damaged_04.wav
+copy fuel_tank_damaged_03.wav fuel_tank_damaged_05.wav
+
+copy gun_damaged_01.wav gun_damaged_03.wav
+copy gun_damaged_02.wav gun_damaged_04.wav
+
+copy gun_destroyed_01.wav gun_destroyed_02.wav
+copy gun_destroyed_02.wav gun_destroyed_03.wav
+
+copy gun_functional_01.wav gun_functional_03.wav
+copy gun_functional_02.wav gun_functional_04.wav
+
+copy gunner_killed_01.wav gunner_killed_03.wav
+copy gunner_killed_02.wav gunner_killed_04.wav
+
+copy loader_killed_01.wav loader_killed_03.wav
+copy loader_killed_02.wav loader_killed_04.wav
+
+copy radio_damaged_01.wav radio_damaged_04.wav
+copy radio_damaged_02.wav radio_damaged_05.wav
+copy radio_damaged_03.wav radio_damaged_06.wav
+
+copy radioman_killed_01.wav radioman_killed_04.wav
+
+copy surveying_devices_damaged_01.wav surveying_devices_damaged_02.wav
+copy surveying_devices_damaged_02.wav surveying_devices_damaged_03.wav
+copy surveying_devices_damaged_03.wav surveying_devices_damaged_04.wav
+copy surveying_devices_damaged_04.wav surveying_devices_damaged_05.wav
+copy surveying_devices_damaged_05.wav surveying_devices_damaged_06.wav
+copy surveying_devices_damaged_06.wav surveying_devices_damaged_07.wav
+
+copy surveying_devices_destroyed_01.wav surveying_devices_destroyed_02.wav
+copy surveying_devices_destroyed_02.wav surveying_devices_destroyed_03.wav
+copy surveying_devices_destroyed_03.wav surveying_devices_destroyed_04.wav
+copy surveying_devices_destroyed_04.wav surveying_devices_destroyed_05.wav
+copy surveying_devices_destroyed_05.wav surveying_devices_destroyed_06.wav
+copy surveying_devices_destroyed_06.wav surveying_devices_destroyed_07.wav
+
+copy surveying_devices_functional_01.wav surveying_devices_functional_02.wav
+copy surveying_devices_functional_02.wav surveying_devices_functional_03.wav
+copy surveying_devices_functional_03.wav surveying_devices_functional_04.wav
+copy surveying_devices_functional_04.wav surveying_devices_functional_05.wav
+copy surveying_devices_functional_05.wav surveying_devices_functional_06.wav
+copy surveying_devices_functional_06.wav surveying_devices_functional_07.wav
+
+copy track_damaged_03.wav track_damaged_04.wav
+
+copy track_functional_01.wav track_functional_02.wav
+copy track_functional_02.wav track_functional_03.wav
+copy track_functional_03.wav track_functional_04.wav
+
+copy track_functional_can_move_01.wav track_functional_can_move_03.wav
+copy track_functional_can_move_02.wav track_functional_can_move_04.wav
+copy track_functional_can_move_03.wav track_functional_can_move_05.wav
+copy track_functional_can_move_04.wav track_functional_can_move_06.wav
+copy track_functional_can_move_05.wav track_functional_can_move_07.wav
+
+copy turret_rotator_damaged_01.wav turret_rotator_damaged_02.wav
+copy turret_rotator_damaged_02.wav turret_rotator_damaged_03.wav
+
+copy turret_rotator_destroyed_01.wav turret_rotator_destroyed_02.wav
+copy turret_rotator_destroyed_02.wav turret_rotator_destroyed_03.wav
+copy turret_rotator_destroyed_03.wav turret_rotator_destroyed_04.wav
